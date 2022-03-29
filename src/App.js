554cecdcc0781c55f0448b45e190594d1ed61baa
= <azazasdfg.com@gmail.com>
2022-03-29 18:52:27 +0300

    Create routing system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import ForgotPassword from './pages/ForgotPassword.tsx';
+import Profile from './pages/Profile.tsx';
+import Explore from './pages/Explore.tsx';
+import SignIn from './pages/SignIn.tsx';
+import SignUp from './pages/SignUp.tsx';
+import Offers from './pages/Offers.tsx';
 
 function App() {
   return (
-    <div className="App">
+    <>
 			<h1>House Marketplace App</h1>
-    </div>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Explore/>}/>
+					<Route path='/offers' element={<Offers/>}/>
+					<Route path='/profile' element={<Profile/>}/>
+					<Route path='/sign-in' element={<SignIn/>}/>
+					<Route path='/sign-up' element={<SignUp/>}/>
+					<Route path='/forgot-password' element={<ForgotPassword/>}/>
+				</Routes>
+			</Router>
+    </>
   );
 }
 
