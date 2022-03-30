ff734fc481726958e018eba631b559ac1d800a77
= <azazasdfg.com@gmail.com>
2022-03-30 18:54:35 +0300

    Add SignIn comonent

--- src/App.js
+++ src/App.js
@@ -10,12 +10,11 @@ import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <>
-			<h1>House Marketplace App</h1>
 			<Router>
 				<Routes>
 					<Route exact path='/' element={<Explore/>}/>
 					<Route exact path='/offers' element={<Offers/>}/>
-					<Route exact path='/profile' element={<Profile/>}/>
+					<Route exact path='/profile' element={<SignIn/>}/>
 					<Route exact path='/sign-in' element={<SignIn/>}/>
 					<Route exact path='/sign-up' element={<SignUp/>}/>
 					<Route exact path='/forgot-password' element={<ForgotPassword/>}/>
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,57 @@
+import { useState } from 'react'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
 export default function SignIn() {
+
+	const [passVisibility, setPassVisibility] = useState(false)
+	const [formData, setFormData] = useState({
+		email: '',
+		password: ''
+	})
+	const { email, password } = formData
+
+	const onChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.id]: e.target.value
+		})
+	}
+
 	return (
-		<div>Sign In</div>
-	)
+		<>
+			<div className="pageContainer">
+				<header>
+					<p className="pageHeader">Welcome Back</p>
+				</header>
+				<form>
+					<input
+						className="emailInput"
+						placeholder="Email"
+						type="email"
+						id="email"
+						value={email}
+						onChange={onChange}
+					/>
+
+					<div className="passwordInputDiv">
+						<input
+							type={passVisibility ? "text" : "password"}
+							className="passwordInput"
+							placeholder="Password"
+							id="password"
+							value={password}
+							onChange={onChange}
+						/>
+
+						<img
+							src={visibilityIcon}
+							alt="show password"
+							className="showPassword"
+							onClick={() => setPassVisibility(!passVisibility)}
+						/>
+					</div>
+				</form>
+			</div>
+		</>
+	);
 }
