a6cf15c1e790c851e2a0cba01390be1b94d5f923
= <azazasdfg.com@gmail.com>
2022-04-01 15:20:29 +0300

    Add "sing in" & "forgot password" links

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,7 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 
 export default function SignIn() {
 
@@ -19,38 +21,56 @@ export default function SignIn() {
 
 	return (
 		<>
-			<div className="pageContainer">
+			<div className='pageContainer'>
 				<header>
-					<p className="pageHeader">Welcome Back</p>
+					<p className='pageHeader'>Welcome Back</p>
 				</header>
 				<form>
 					<input
-						className="emailInput"
-						placeholder="Email"
-						type="email"
-						id="email"
+						className='emailInput'
+						placeholder='Email'
+						type='email'
+						id='email'
 						value={email}
 						onChange={onChange}
 					/>
 
-					<div className="passwordInputDiv">
+					<div className='passwordInputDiv'>
 						<input
-							type={passVisibility ? "text" : "password"}
-							className="passwordInput"
-							placeholder="Password"
-							id="password"
+							type={passVisibility ? 'text' : 'password'}
+							className='passwordInput'
+							placeholder='Password'
+							id='password'
 							value={password}
 							onChange={onChange}
 						/>
 
 						<img
 							src={visibilityIcon}
-							alt="show password"
-							className="showPassword"
+							alt='show password'
+							className='showPassword'
 							onClick={() => setPassVisibility(!passVisibility)}
 						/>
+
+						<Link to='/forgot-password' className='forgotPasswordLink'>
+							Forgot password?
+						</Link>
+
+						<div className='signInBar'>
+							<p className='signInText'>Sign In</p>
+							<button className='signInButton'>
+								<ArrowRightIcon fill='#ffffff' width='34px' height='34px'>
+								</ArrowRightIcon>
+							</button>
+						</div>
 					</div>
 				</form>
+
+				{/* google auth here */}
+
+				<Link to='/sign-up' className='registerLink'>
+					Sign Up Instead
+				</Link>
 			</div>
 		</>
 	)
